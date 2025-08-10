21f38d6007d33523e280e068e188fa24744ce2f9
David <david11jv@gmail.com>
2025-08-10 11:24:20 +0200

    Initialize API Gateway and Employee Service microservices with Eureka Discovery and Maven Wrapper configuration

---
+++ employee-service/src/main/java/com/davinchicoder/employee_service/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.davinchicoder.employee_service.controller;
+
+import com.davinchicoder.employee_service.model.Employee;
+import com.davinchicoder.employee_service.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> findAll() {
+        return ResponseEntity.ok(employeeRepository.findAll());
+    }
+
+    @GetMapping("/department/{id}")
+    public ResponseEntity<List<Employee>> findAllByDepartmentId(@PathVariable Long id) {
+        return ResponseEntity.ok(employeeRepository.findByDepartmentId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody Employee employee) {
+        employeeRepository.create(employee);
+        return ResponseEntity.created(URI.create("/api/v1/employees/".concat(employee.getId().toString()))).build();
+    }
+
+
+}
